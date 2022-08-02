5c77ad649dd2ba1573078afe99c5e1ff69a003cd
jhasanovic <jhasanovic1@etf.unsa.ba>
2022-08-03 01:17:44 +0200

    Prva migracija na bazu

--- OOAD tutorijal FINAL/Data/ApplicationDbContext.cs
+++ OOAD tutorijal FINAL/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OOAD_tutorijal_FINAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,16 @@ namespace OOAD_tutorijal_FINAL.Data
             : base(options)
         {
         }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<UpisNaPredmet> UpisNaPredmet { get; set; }
+        public DbSet<Predmet> Predmet { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<UpisNaPredmet>().ToTable("UpisNaPredmet");
+            modelBuilder.Entity<Predmet>().ToTable("Predmet");
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
