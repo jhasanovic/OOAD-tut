fb81cbfcb9e30ea7137abfc1d2bfcb57c1f0f8ba
jhasanovic <jhasanovic1@etf.unsa.ba>
2022-08-02 17:23:31 +0200

    Initial commit

---
+++ OOAD tutorijal FINAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOAD_tutorijal_FINAL.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ OOAD tutorijal FINAL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OOAD_tutorijal_FINAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
